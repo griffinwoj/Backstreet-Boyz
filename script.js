64f5ca8b6f946c59e3de6aecfe0575377bf7adaf
griffinw@protonmail.com <griffinw@protonmail.com>
2023-03-20 19:31:52 -0400

    third commit

--- script.js
+++ script.js
@@ -1,80 +1,220 @@
+// const googleMapsApiKey = 'AIzaSyAaqlOLHQvJj6oZnB8CPCgLvWLZttkeZjc';
+// // Create the script tag, set the appropriate attributes
+// var script = document.createElement('script');
+// script.src = `https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}&callback=initMap`;
+// script.async = true;
 
-// more trials
+// // Attach your callback function to the `window` object
+// window.initMap = function() {
 
-  const corsHeaders = {
-    "Access-Control-Allow-Origin": "https://your-website-domain.com",
-    "Access-Control-Allow-Methods": "GET",
-    "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
-    "Access-Control-Max-Age": "3600"
-  };
+// };
+
+// // Append the 'script' element to 'head'
+// document.head.appendChild(script);
+// // // more trials
+
+//   const corsHeaders = {
+//     "Access-Control-Allow-Origin": "https://your-website-domain.com",
+//     "Access-Control-Allow-Methods": "GET",
+//     "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
+//     "Access-Control-Max-Age": "3600"
+//   };
+//   const corsUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=";
   
-  const corsUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=";
-  const googleMapsApiKey = 'AIzaSyAaqlOLHQvJj6oZnB8CPCgLvWLZttkeZjc';
-  const trailApiKey = 'fc6aecdb0mshaa821d0dd2c6c0ap1f7de4jsncf7d761a6010';
+//   const trailApiKey = 'fc6aecdb0mshaa821d0dd2c6c0ap1f7de4jsncf7d761a6010';
   
-  navigator.geolocation.getCurrentPosition(position => {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
+//   navigator.geolocation.getCurrentPosition(position => {
+//     const latitude = position.coords.latitude;
+//     const longitude = position.coords.longitude;
   
-    fetch(`${corsUrl}${latitude},${longitude}&radius=500&type=cafe&keyword=coffee&key=${googleMapsApiKey}`)
-      .then(response => response.json())
-      .then(data => {
-        const coffeeShop = data.results[0];
-        const coffeeShopName = coffeeShop.name;
-        const coffeeShopAddress = coffeeShop.vicinity;
+//     fetch(`${corsUrl}${latitude},${longitude}&radius=500&type=cafe&keyword=coffee&key=${googleMapsApiKey}`)
+//       .then(response => response.json())
+
+//       .then(data => {
+//         const coffeeShop = data.results[0];
+//         const coffeeShopName = coffeeShop.name;
+//         const coffeeShopAddress = coffeeShop.vicinity;
   
-        const coffeeShopElement = document.createElement('div');
-        coffeeShopElement.innerHTML = `Nearest coffee shop: ${coffeeShopName} (${coffeeShopAddress})`;
-        document.body.appendChild(coffeeShopElement);
-      });
+//         const coffeeShopElement = document.createElement('div');
+//         coffeeShopElement.innerHTML = `Nearest coffee shop: ${coffeeShopName} (${coffeeShopAddress})`;
+//         document.body.appendChild(coffeeShopElement);
+//       });
   
-    fetch(`https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=10&key=${trailApiKey}`)
-      .then(response => response.json())
-      .then(data => {
-        const hikingTrail = data.trails[0];
-        const hikingTrailName = hikingTrail.name;
-        const hikingTrailLocation = hikingTrail.location;
-        const hikingTrailLength = hikingTrail.length;
+//     fetch(`https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=10&key=${trailApiKey}`)
+//       .then(response => response.json())
+//       .then(data => {
+//         const hikingTrail = data.trails[0];
+//         const hikingTrailName = hikingTrail.name;
+//         const hikingTrailLocation = hikingTrail.location;
+//         const hikingTrailLength = hikingTrail.length;
   
-        const hikingTrailElement = document.createElement('div');
-        hikingTrailElement.innerHTML = `Nearest hiking trail: ${hikingTrailName} (${hikingTrailLocation}, ${hikingTrailLength} miles)`;
-        document.body.appendChild(hikingTrailElement);
-      });
+//         const hikingTrailElement = document.createElement('div');
+//         hikingTrailElement.innerHTML = `Nearest hiking trail: ${hikingTrailName} (${hikingTrailLocation}, ${hikingTrailLength} miles)`;
+//         document.body.appendChild(hikingTrailElement);
+//       });
   
-    const map = new google.maps.Map(document.getElementById("map"));
-    const infoWindow = new google.maps.InfoWindow();
-    const locationButton = document.createElement("button");
+//     const map = new google.maps.Map(document.getElementById("map"));
+//     const infoWindow = new google.maps.InfoWindow();
+//     const locationButton = document.createElement("button");
+//   });
+  
+//   function initMap() {
+//     const map = new google.maps.Map(document.getElementById("map"));
+//     const infoWindow = new google.maps.InfoWindow();
+//     const locationButton = document.createElement("button");
+//     locationButton.textContent = "Pan to Current Location";
+//     locationButton.classList.add("custom-map-control-button");
+//     map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+//     locationButton.addEventListener("click", () => {
+//       // Try HTML5 geolocation.
+//       if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(
+//           (position) => {
+//             const pos = {
+//               lat: position.coords.latitude,
+//               lng: position.coords.longitude,
+//             };
+//             infoWindow.setPosition(pos);
+//             infoWindow.setContent("Location found.");
+//             infoWindow.open(map);
+//             map.setCenter(pos);
+//           },
+//           () => {
+//             handleLocationError(true, infoWindow, map.getCenter());
+//           }
+//         );
+//       } else {
+//         // Browser doesn't support Geolocation
+//         handleLocationError(false, infoWindow, map.getCenter());
+//       }
+//     });
+//   }
+  
+//   // google.maps.event.addDomListener(window, 'load', initMap);
+
+//   // const corsHeaders = {
+//   //   "Access-Control-Allow-Origin": "https://your-website-domain.com",
+//   //   "Access-Control-Allow-Methods": "GET",
+//   //   "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
+//   //   "Access-Control-Max-Age": "3600"
+//   // };
+  
+//   // const corsUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=";
+  
+//   fetch('/maps/api/place/nearbysearch?latitude=' + latitude + '&longitude=' + longitude)
+//   .then(response => response.json())
+//   .then(data => {
+//     // process the data
+//   });
+//   function initMap() {
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//       center: { lat: 37.7749, lng: -122.4194 },
+//       zoom: 8,
+//     });
+//     const infoWindow = new google.maps.InfoWindow();
+//     const locationButton = document.createElement("button");
+//     locationButton.textContent = "Pan to Current Location";
+//     locationButton.classList.add("custom-map-control-button");
+//     map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+//     locationButton.addEventListener("click", () => {
+//       // Try HTML5 geolocation.
+//       if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(
+//           (position) => {
+//             const pos = {
+//               lat: position.coords.latitude,
+//               lng: position.coords.longitude,
+//             };
+//             infoWindow.setPosition(pos);
+//             infoWindow.setContent("Location found.");
+//             infoWindow.open(map);
+//             map.setCenter(pos);
+//           },
+//           () => {
+//             handleLocationError(true, infoWindow, map.getCenter());
+//           }
+//         );
+//       } else {
+//         // Browser doesn't support Geolocation
+//         handleLocationError(false, infoWindow, map.getCenter());
+//       }
+//     });
+//   }
+  const googleMapsApiKey = ''AIzaSyAaqlOLHQvJj6oZnB8CPCgLvWLZttkeZjc'';
+const corsUrl = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=";
+const trailApiKey = ''fc6aecdb0mshaa821d0dd2c6c0ap1f7de4jsncf7d761a6010'';
+
+// Create the script tag, set the appropriate attributes
+const script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}&callback=initMap`;
+script.async = true;
+
+// Attach your callback function to the `window` object
+window.initMap = function() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.7749, lng: -122.4194 },
+    zoom: 8,
   });
-  
-  function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"));
-    const infoWindow = new google.maps.InfoWindow();
-    const locationButton = document.createElement("button");
-    locationButton.textContent = "Pan to Current Location";
-    locationButton.classList.add("custom-map-control-button");
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-    locationButton.addEventListener("click", () => {
-      // Try HTML5 geolocation.
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            const pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude,
-            };
-            infoWindow.setPosition(pos);
-            infoWindow.setContent("Location found.");
-            infoWindow.open(map);
-            map.setCenter(pos);
-          },
-          () => {
-            handleLocationError(true, infoWindow, map.getCenter());
-          }
-        );
-      } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-      }
+  const infoWindow = new google.maps.InfoWindow();
+  const locationButton = document.createElement("button");
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+};
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
+
+navigator.geolocation.getCurrentPosition(position => {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  fetch(`${corsUrl}${latitude},${longitude}&radius=500&type=cafe&keyword=coffee&key=${googleMapsApiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      const coffeeShop = data.results[0];
+      const coffeeShopName = coffeeShop.name;
+      const coffeeShopAddress = coffeeShop.vicinity;
+
+      const coffeeShopElement = document.createElement('div');
+      coffeeShopElement.innerHTML = `Nearest coffee shop: ${coffeeShopName} (${coffeeShopAddress})`;
+      document.body.appendChild(coffeeShopElement);
     });
-  }
-  google.maps.event.addDomListener(window, 'load', initMap);
+
+  fetch(`https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=10&key=${trailApiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      const hikingTrail = data.trails[0];
+      const hikingTrailName = hikingTrail.name;
+      const hikingTrailLocation = hikingTrail.location;
+      const hikingTrailLength = hikingTrail.length;
+
+      const hikingTrailElement = document.createElement('div');
+      hikingTrailElement.innerHTML = `Nearest hiking trail: ${hikingTrailName} (${hikingTrailLocation}, ${hikingTrailLength} miles)`;
+      document.body.appendChild(hikingTrailElement);
+    });
+
+});
